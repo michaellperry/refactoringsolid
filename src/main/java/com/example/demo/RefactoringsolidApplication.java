3243f25f21b702e6a15aabc9391cc6c199a2133d
Michael L Perry <michael@qedcode.com>
2025-05-18 13:21:17 -0500

    Removed unused imports

--- src/main/java/com/example/demo/RefactoringsolidApplication.java
+++ src/main/java/com/example/demo/RefactoringsolidApplication.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import com.example.demo.service.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
--- src/test/java/com/example/demo/service/ReportServiceTest.java
+++ src/test/java/com/example/demo/service/ReportServiceTest.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
 /**
